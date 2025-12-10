da5fa4132966db9a514c3278221fad2fa25bdfd9
Kushal rathore <132640976+199904092023@users.noreply.github.com>
2025-12-10 16:30:10 +0530

    changed avatar color

--- frontend/src/components/Avatar.jsx
+++ frontend/src/components/Avatar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Avatar = ({username}) => {
   return (
-    <div className='rounded-full text-sm md:text-base lg:text-lg  font-medium py-0 px-3  md:py-2 md:px-4 bg-slate-400 text-white flex justify-center items-center h-full' >{username.charAt(0)}</div>
+    <div className='rounded-full text-sm md:text-base lg:text-lg  font-medium py-0 px-3  md:py-2 md:px-4 bg-slate-300 text-white flex justify-center items-center h-full' >{username.charAt(0)}</div>
   )
 }
 
